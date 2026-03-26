01bfedb3a5fe5af5f8e61685305f5e48d3ac79a3
Jeffry Leon <ryffje@gmail.com>
2026-03-26 12:32:54 -0600

    chore: initial Cypress setup for BretLab QA automation project

---
+++ cypress/e2e/register.cy.js
@@ -0,0 +1,52 @@
+describe('BretLab - Registro', () => {
+  beforeEach(() => {
+    cy.visit('https://www.bretlab.net/');
+
+    cy.contains('button, a', /registr/i, { timeout: 10000 })
+      .should('be.visible')
+      .click({ force: true });
+  });
+
+  it('debe escribir en el campo nombre', () => {
+    cy.get('#firstName')
+      .should('be.visible')
+      .clear()
+      .type('Jeffry')
+      .should('have.value', 'Jeffry');
+  });
+
+  it('debe escribir en el campo apellido', () => {
+    cy.get('#lastName')
+      .should('be.visible')
+      .clear()
+      .type('Leon')
+      .should('have.value', 'Leon');
+  });
+
+  it('debe validar que el email exista', () => {
+    cy.get('input[name="email"], #email')
+      .should('be.visible');
+  });
+
+  it('debe mostrar validaciones al intentar enviar vacío', () => {
+    cy.get('button[type="submit"]')
+      .scrollIntoView()
+      .click({ force: true });
+
+    cy.contains('Este campo es obligatorio').should('be.visible');
+  });
+
+  it('debe llenar el formulario con datos válidos desde fixture', () => {
+    cy.fixture('user').then((data) => {
+      const user = data.validUser;
+
+      cy.get('#firstName').clear().type(user.firstName);
+      cy.get('#lastName').clear().type(user.lastName);
+      cy.get('input[name="email"], #email').clear().type(user.email);
+      cy.get('input[name="password"], #password').clear().type(user.password);
+      cy.get('input[name="phone"], #phone').clear().type(user.phone);
+      cy.get('input[name="documentId"], #documentId').clear().type(user.documentId);
+      cy.get('input[name="location"], #location').clear().type(user.location);
+    });
+  });
+});
